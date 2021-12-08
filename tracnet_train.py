9428ad291af221e879416324e7667b641f33c976
Alexander Richard <richard@cip.ifi.lmu.de>
2021-12-08 01:32:11 +0100

    Plotting metrics and small adjustments

--- tracnet_train.py
+++ tracnet_train.py
@@ -2,24 +2,20 @@ import numpy as np
 from scipy.io import loadmat
 import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.models import Model
 from tensorflow.keras.losses import MeanSquaredError
-from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.metrics import Accuracy, RootMeanSquaredError, MeanAbsoluteError, MeanIoU
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tracnet_model import build_tracnet
 import os
-#from customized_lr_schedules import StepDecay
-from math import pow, floor
-from matplotlib import pyplot
-import sklearn
-from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 # set seed for reproducability
 np.random.seed(1)
 tf.compat.v1.set_random_seed(1)
 
+
 # create input and target datasets
-def create_datasets(size, input_data_filepath, target_date_filepath):
+def create_datasets(size, input_data_filepath, target_data_filepath):
     dspl_array = np.empty([1472, size, size, 2], dtype='float64')
     for i, filename in enumerate(os.listdir(input_data_filepath)):
         f = os.path.join(input_data_filepath, filename)
@@ -28,51 +24,86 @@ def create_datasets(size, input_data_filepath, target_date_filepath):
             dspl_array[i, :, :, :] = file['dspl']
 
     trac_array = np.empty([1472, size, size, 2], dtype='float64')
-    for i, filename in enumerate(os.listdir(target_date_filepath)):
-        f = os.path.join(target_date_filepath, filename)
+    for i, filename in enumerate(os.listdir(target_data_filepath)):
+        f = os.path.join(target_data_filepath, filename)
         if os.path.isfile(f):
             file = loadmat(f)
             trac_array[i, :, :, :] = file['trac']
 
-
     return dspl_array, trac_array
+
 
 # step-wise decay
 def lr_step_decay(epoch):
     initial_lr = 6e-4
     drop_rate = 0.7943
     epochs_drop = 10.0
-    return initial_lr * pow(drop_rate, floor(epoch/epochs_drop))
+    return initial_lr * np.power(drop_rate, np.floor(epoch/epochs_drop))
 
 
 # build and train a tracnet from scratch
 def train_tracnet(X_train, y_train, input_shape=(104, 104, 2), epochs=5):
-    model = build_tracnet(input_shape)
+    mag = input_shape[0] / 104
+    batch_size = np.ceil(30 / mag)
+    model = build_tracnet(input_shape, batch_size)
     model.compile(
         optimizer=SGD(momentum=0.9),
         loss=MeanSquaredError(),
-        metrics=[RootMeanSquaredError(), 'mse', 'mae']
+        metrics=[Accuracy(), RootMeanSquaredError(), MeanAbsoluteError(), MeanIoU()]
     )
     model.summary()
     history = model.fit(
         X_train,
         y_train,
-        batch_size=32,
+        validation_split=0.1,
+        batch_size=batch_size,
+        shuffle=True,
         epochs=epochs,
         callbacks=[LearningRateScheduler(lr_step_decay, verbose=2)],
         verbose=2
     )
-    pyplot.plot(history.history['root_mean_squared_error'])
-#    pyplot.plot(history.history['mean_squared_error'])
-#    pyplot.plot(history.history['mean_absolute_percentage_error'])
-    pyplot.show()
-    pyplot.savefig('fig.png')
+    # plot accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig('accuracy.png')
+
+    # plot loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig('loss.png')
+
+    # plot rmse
+    plt.plot(history.history['root_mean_squared_error'])
+    plt.plot(history.history['val_root_mean_squared_error'])
+    plt.title('model rmse')
+    plt.ylabel('rmse')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig('rmse.png')
+
+    # plot meanIoU
+    plt.plot(history.history['mean_iou'])
+    plt.plot(history.history['val_mean_iou'])
+    plt.title('model mean_iou')
+    plt.ylabel('mean_iou')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig('mean_iou.png')
+
+    # save model
     model.save(
         "'model.tf'",
         save_format='tf',
         include_optimizer=True
     )
-    pyplot.close()
 
 
 X_train, y_train = create_datasets(104, '/home/r/richard/Desktop/trainData104/dspl',
